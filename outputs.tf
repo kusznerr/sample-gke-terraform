b215e80907e3d1871adcf8e23bcd54e05b9070e9
kusznerr <kuszner@gmail.com>
2021-11-19 09:46:37 +0100

    enable kbeconfig output

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,26 @@ output "cluster_name" {
   description = "Cluster name"
   value       = module.gke.name
 }
+
+output "kubernetes_endpoint" {
+  sensitive = true
+  value     = module.gke_auth.host
+}
+
+output "client_token" {
+  sensitive = true
+  value     = module.gke_auth.token
+}
+
+output "ca_certificate" {
+  value = module.gke_auth.cluster_ca_certificate
+}
+
+output "kubeconfig_raw" {
+  value = module.gke_auth.kubeconfig_raw
+}
+
+output "service_account" {
+  description = "The default service account used for running nodes."
+  value       = module.gke.service_account
+}
