02017b461ae121c7da3acde371b3d368611a9d1d
kusznerr <kuszner@gmail.com>
2021-11-19 23:11:28 +0100

    Enable ArgoCD

--- main.tf
+++ main.tf
@@ -104,3 +104,33 @@ module "gke" {
     ]
   }
 }
+
+provider "kubectl" {
+  host                   = module.gke_auth.host
+  cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+  token                  = module.gke_auth.token
+  load_config_file       = false
+}
+
+data "kubectl_file_documents" "namespace" {
+    content = file("./manifests/argocd/namespace.yaml")
+} 
+
+data "kubectl_file_documents" "argocd" {
+    content = file("./manifests/argocd/install.yaml")
+}
+
+resource "kubectl_manifest" "namespace" {
+    count     = length(data.kubectl_file_documents.namespace.documents)
+    yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
+    override_namespace = "argocd"
+}
+
+resource "kubectl_manifest" "argocd" {
+    depends_on = [
+      kubectl_manifest.namespace,
+    ]
+    count     = length(data.kubectl_file_documents.argocd.documents)
+    yaml_body = element(data.kubectl_file_documents.argocd.documents, count.index)
+    override_namespace = "argocd"
+}
