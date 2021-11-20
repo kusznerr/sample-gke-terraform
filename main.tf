ec63f9302f83aabb08b3e3e7ccfdb1fa2adc9d6d
kusznerr <kuszner@gmail.com>
2021-11-20 16:29:26 +0100

    Enable argoCD

--- main.tf
+++ main.tf
@@ -113,3 +113,36 @@ module "gke" {
     ]
   }
 }
+
+// Enable ArgoCD server
+// Any changes to this configuration ie. destroy / refresh throws terraform errors
+
+provider "kubectl" {
+  host                   = module.gke_auth.host
+  cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+  token                  = module.gke_auth.token
+  load_config_file       = false
+}
+
+data "kubectl_file_documents" "namespace" {
+    content = file("./manifests/argocd/namespace.yaml")
+} 
+
+data "kubectl_file_documents" "argocd" {
+    content = file("./manifests/argocd/install.yaml")
+}
+
+resource "kubectl_manifest" "namespace" {
+    count     = length(data.kubectl_file_documents.namespace.documents)
+    yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
+    override_namespace = "argocd"
+}
+
+resource "kubectl_manifest" "argocd" {
+    depends_on = [
+      kubectl_manifest.namespace,
+    ]
+    count     = length(data.kubectl_file_documents.argocd.documents)
+    yaml_body = element(data.kubectl_file_documents.argocd.documents, count.index)
+    override_namespace = "argocd"
+}
