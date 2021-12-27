bbd4b1f2889d904018824922d689ef08d82bc8d2
kusznerr <kuszner@gmail.com>
2021-12-27 17:23:12 +0100

    Another change

--- argo/main.tf
+++ argo/main.tf
@@ -1,12 +1,8 @@
 terraform {
   required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = ">= 2.0.3"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.1.0"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
     }
   }
 }
@@ -22,31 +18,62 @@ data "terraform_remote_state" "gke" {
   }
 }
 
-# resource "local_file" "kubeconfig" {
-#   content  = data.terraform_remote_state.gke.outputs.kubeconfig_raw
-#   filename = "~/.kube/config"
+provider "kubectl" {
+  host                   = data.terraform_remote_state.gke.outputs.kubernetes_endpoint
+  cluster_ca_certificate = data.terraform_remote_state.gke.outputs.ca_certificat
+  token                  = data.terraform_remote_state.gke.outputs.client_token
+  load_config_file       = false
+}
+
+data "kubectl_file_documents" "namespace" {
+    content = file("./yaml/namespace.yaml")
+} 
+
+data "kubectl_file_documents" "argocd" {
+    content = file("./yaml/install.yaml")
+}
+
+data "kubectl_file_documents" "root-app" {
+    content = file("./yaml/root-app.yaml")
+}
+
+resource "kubectl_manifest" "namespace" {
+    count     = length(data.kubectl_file_documents.namespace.documents)
+    yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
+    override_namespace = "argocd"
+}
+
+resource "kubectl_manifest" "argocd" {
+    depends_on = [
+      kubectl_manifest.namespace,
+    ]
+    count     = length(data.kubectl_file_documents.argocd.documents)
+    yaml_body = element(data.kubectl_file_documents.argocd.documents, count.index)
+    override_namespace = "argocd"
+}
+
+resource "kubectl_manifest" "root-app" {
+    depends_on = [
+      kubectl_manifest.argocd,
+    ]
+    count     = length(data.kubectl_file_documents.apps.documents)
+    yaml_body = element(data.kubectl_file_documents.apps.documents, count.index)
+    override_namespace = "argocd"
+}
+
+
+# resource helm_release argocd-install {
+#   name       = "argo-cd"
+#   repository = "https://kusznerr.github.io/wabbit-rk5-gke-argo-apps"
+#   chart      = "argo-cd"
+#   version    = "1.0.4"
 # }
 
-provider "helm" {
-  kubernetes {
-    host  = data.terraform_remote_state.gke.outputs.kubernetes_endpoint
-    token = data.terraform_remote_state.gke.outputs.client_token
-    cluster_ca_certificate = data.terraform_remote_state.gke.outputs.ca_certificate
-  }
-}
-
-resource helm_release argocd-install {
-  name       = "argo-cd"
-  repository = "https://kusznerr.github.io/wabbit-rk5-gke-argo-apps"
-  chart      = "argo-cd"
-  version    = "1.0.4"
-}
-
-resource helm_release root-app {
-    depends_on = [
-      helm_release.argocd-install,
-    ]
-  name       = "root-app"
-  repository = "https://kusznerr.github.io/wabbit-rk5-gke-argo-apps"
-  chart      = "root-app"
-}
+# resource helm_release root-app {
+#     depends_on = [
+#       helm_release.argocd-install,
+#     ]
+#   name       = "root-app"
+#   repository = "https://kusznerr.github.io/wabbit-rk5-gke-argo-apps"
+#   chart      = "root-app"
+# }
